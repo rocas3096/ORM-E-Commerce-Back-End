90bf5f00b56f791afdf810a2cd7a9d015c1de3af
rocas3096 <ozavala3096@gmail.com>
2023-05-26 17:33:08 -0500

    Update category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -87,7 +87,7 @@ router.delete("/:id", async (req, res) => {
       return;
     }
 
-    res.status(200).json(categoryData);
+    res.status(200).json("Category has been deleted.");
   } catch (err) {
     res.status(500).json(err);
   }
