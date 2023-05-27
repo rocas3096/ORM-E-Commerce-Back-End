fb6274073fcf017161549dd26fe98052f86b6414
rocas3096 <ozavala3096@gmail.com>
2023-05-27 06:25:23 -0500

    Update product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include:[Category, Tag,],
+      include: [{ model: Category }, { model: Tag}],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -23,11 +23,11 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include:[Category, Tag],
+      include: [{ model: Category }, { model: Tag}],
     });
     
     if(!productData) {
-      res.status(400).json({message: 'No product data fround with that id!'});
+      res.status(400).json({message: 'No product data found with that id!'});
       return;
     }
 
@@ -38,7 +38,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -47,7 +47,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  await Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -70,9 +70,9 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
-  Product.update(req.body, {
+  await Product.update(req.body, {
     where: {
       id: req.params.id,
     },
