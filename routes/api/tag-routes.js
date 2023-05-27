c0f53bee33692c0943b04f3e824e288d7d8d9b63
rocas3096 <ozavala3096@gmail.com>
2023-05-27 07:39:03 -0500

    Update tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,31 +1,33 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData  = await Tag.findAll({
-      include:[Product]
+    const tagData = await Tag.findAll({
+      include: [Product],
     });
-    res.status(200).json(tagData );
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include:[Product],
+      include: [Product],
     });
-    
-    if(!tagData) {
-      res.status(400).json({message: 'No catergory tag fround with that id!'});
+
+    if (!tagData) {
+      res
+        .status(400)
+        .json({ message: "No catergory tag fround with that id!" });
       return;
     }
 
@@ -35,7 +37,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
   try {
     const tagData = await Tag.create({
@@ -48,25 +50,34 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.findByPk(req.params.id, {
-      include: [Product],
-    });
+    const [updatedRowCount] = await Tag.update(
+      { tag_name: req.body.tag_name },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
 
-    if(!tagData) {
-      res.status(400).json({ message: 'No tag data found with that id!'});
+    if (updatedRowCount === 0) {
+      res.status(404).json({ message: "No tag found with that id!" });
       return;
     }
 
-    res.status(200).json(tagData);
-  } catch(err) {
+    const updatedTag = await Tag.findByPk(req.params.id, {
+      include: [Product],
+    });
+
+    res.status(200).json(updatedTag);
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
   try {
     const tagData = await Tag.destroy({
@@ -76,7 +87,7 @@ router.delete('/:id', async (req, res) => {
     });
 
     if (!tagData) {
-      res.status(404).json({ message: 'No tag found with that id!' });
+      res.status(404).json({ message: "No tag found with that id!" });
       return;
     }
 
