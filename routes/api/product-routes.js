3505ff4ca357fec257c83bfa108c24a8eb15495d
rocas3096 <ozavala3096@gmail.com>
2023-06-02 14:10:08 -0500

    Update product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,15 +1,15 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag}],
+      include: [{ model: Category }, { model: Tag }],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -18,16 +18,16 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag}],
+      include: [{ model: Category }, { model: Tag }],
     });
-    
-    if(!productData) {
-      res.status(400).json({message: 'No product data found with that id!'});
+
+    if (!productData) {
+      res.status(400).json({ message: "No product data found with that id!" });
       return;
     }
 
@@ -38,7 +38,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -70,7 +70,7 @@ router.post('/', async (req, res) => {
 });
 
 // update product
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   // update product data
   await Product.update(req.body, {
     where: {
@@ -111,7 +111,7 @@ router.put('/:id', async (req, res) => {
     });
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
   try {
     const productData = await Product.destroy({
@@ -121,11 +121,11 @@ router.delete('/:id', async (req, res) => {
     });
 
     if (!productData) {
-      res.status(404).json({ message: 'No product found with that id!' });
+      res.status(404).json({ message: "No product found with that id!" });
       return;
     }
 
-    res.status(200).json(productData);
+    res.status(200).json("Product has been deleted.");
   } catch (err) {
     res.status(500).json(err);
   }
